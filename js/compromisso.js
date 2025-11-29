42dad861a1c4ccfc3e57b95f4671e95114b28702
Jonatan_Silva <142622165+JonatanHZ@users.noreply.github.com>
2025-11-28 21:55:27 -0300

    Add files via upload

---
+++ js/compromisso.js
@@ -0,0 +1,13 @@
+function enviar() {
+      const tipo = document.getElementById("tipo").value;
+      const data = document.getElementById("data").value;
+      const hora = document.getElementById("hora").value;
+      const desc = document.getElementById("desc").value;
+
+      if (!tipo || !data || !hora || !desc) {
+        alert("Preencha todos os campos!");
+        return;
+      } else { 
+        alert("Compromisso salvo com sucesso!");
+        }
+    }
